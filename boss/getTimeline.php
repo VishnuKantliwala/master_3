bf14f32a1b417c75446daba56677dd2c5cb66674
Vishnu <vkantliwala@gmail.com>
2020-09-02 09:48:13 +0530

    reassign error fixed

--- boss/getTimeline.php
+++ boss/getTimeline.php
@@ -4,8 +4,13 @@ session_start();
 $cn=new connect();
 $cn->connectdb();
 
-$filter = "";
+function secondsToTime($seconds) {
+    $dtF = new \DateTime('@0');
+    $dtT = new \DateTime("@$seconds");
+    return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes');
+}
 
+$filter = "";
 $task_emp_id = $_POST['task_emp_id'];
 
 $qry = "SELECT t.*,te.*, u.user_id, u.user_name FROM tbl_task AS t, tbl_task_emp AS te, tbl_user u WHERE t.task_id = te.task_id AND te.user_id = u.user_id AND te.task_emp_id =".$task_emp_id;
@@ -27,7 +32,7 @@ if(mysqli_num_rows($sql) > 0)
         $returnObj[$i]["date_accept"]=$row['date_accept'];
         $returnObj[$i]["date_submit"]=$row['date_submit'];       
         $returnObj[$i]["task_emp_status"]=$row['task_emp_status'];       
-        $returnObj[$i]["task_emp_duration"]=$row['task_emp_duration'];       
+        $returnObj[$i]["task_emp_duration"]=secondsToTime($row['task_emp_duration']);  
         
         $i++;
     }
--- boss/reappointTask.php
+++ boss/reappointTask.php
@@ -12,7 +12,10 @@ $cn = new connect();
 $cn->connectdb();
 $task_emp_id = $_POST['task_emp_id'];
 
-$sql = "UPDATE tbl_task_emp SET task_emp_status=3, task_emp_quantity_done=0 WHERE task_emp_id='" . $task_emp_id . "'";
+$sql = "UPDATE tbl_task_emp SET task_emp_status=0, task_emp_quantity_done=0 WHERE task_emp_id='" . $task_emp_id . "'";
+$cn->insertdb($sql);
+
+$sql = "UPDATE tbl_task_emp_qty SET task_emp_status=0 WHERE task_emp_id='" . $task_emp_id . "'";
 $cn->insertdb($sql);
 
 if (mysqli_affected_rows($cn->getConnection()) > 0) {
